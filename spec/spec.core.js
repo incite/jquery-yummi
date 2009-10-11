7ca40674730adac7587237d82ef51836ca8113fd
Julio Cesar Ody <julio.ody@nexuspoint.com.au>
2009-10-12 09:36:00 +1100

    couple more specs

--- spec/spec.core.js
+++ spec/spec.core.js
@@ -1,5 +1,4 @@
-describe 'jquery.yummi'
-  
+describe 'jquery.yummi'  
   before_each
     field = $(fixture('input'))
     field.appendTo('body').show();
@@ -9,6 +8,21 @@ describe 'jquery.yummi'
     field.hide()
   end
   
+  describe 'String.prototype.substrUntil'
+    it 'should return juice if it receives 4 and a whitespace as arguments'
+      string = "bluejuice rocks"
+      string.substrUntil(4, ' ').should.be 'juice'
+    end
+  end
+  
+  describe '$.fn.setCursorPosition'
+    it 'should set the caret position to 2 when 2 is passed as an argument'
+      field.val('testing')
+      field.setCursorPosition(2)
+      field.caretPos().should.be 2
+    end  
+  end
+  
   describe '$.fn.wordAtCaret'
     it 'should be "foo" when the caret is over this word'
       field.val('foo bar nice');
@@ -16,5 +30,4 @@ describe 'jquery.yummi'
       field.wordAtCaret().should.be 'foo'
     end
   end
-  
 end
